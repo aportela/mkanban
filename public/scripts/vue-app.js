9b7b1891c093d4986cd423ed4cc11b1d6259e904
alex <766f6964+github@gmail.com>
2018-03-31 16:47:42 +0200

    added main app (vuejs) component

---
+++ public/scripts/vue-app.js
@@ -0,0 +1,12 @@
+
+/**
+ * main app component
+ */
+const app = new Vue({
+    data: function () {
+        return ({
+        });
+    }, created: function () {
+        console.log("it works!");
+    }
+}).$mount('#app');
