209a6db6a56e6c582a1b2b4fc1e1b5cad8576981
alex <766f6964+github@gmail.com>
2018-04-01 16:06:52 +0200

    moved drag & drop board logic from app to board component

--- public/scripts/vue-app.js
+++ public/scripts/vue-app.js
@@ -9,7 +9,7 @@ const bus = new Vue();
 const app = new Vue({
     data: function () {
         return ({
-            board:
+            boards: [
                 {
                     lists: [
                         {
@@ -53,40 +53,9 @@ const app = new Vue({
                         }
                     ]
                 }
+            ]
         });
     }, created: function () {
         console.log("[app]: created");
-
-        var self = this;
-        // event (dragula): card dropped
-        bus.$on("dragulaDrop", function (card, fromList, toList) {
-            console.log("[app]: event dragulaDrop")
-            var obj = null;
-            var sourceListIdx = -1;
-            var sourceCardIdx = -1;
-            var destCardIdx = 0;
-            // get card object from source list
-            for (var i = 0, notFound = true; i < self.board.lists.length && notFound; i++) {
-                if (self.board.lists[i].id == fromList) { // source list
-                    sourceListIdx = i;
-                    for (var j = 0; j < self.board.lists[i].cards.length && notFound; j++) {
-                        if (self.board.lists[i].cards[j].id == card) { // card on source list
-                            sourceCardIdx = j;
-                            obj = self.board.lists[i].cards[j];
-                            notFound = false;
-                        }
-                    }
-                }
-            }
-            if (obj) {
-                for (var i = 0, notFound = true; i < self.board.lists.length && notFound; i++) {
-                    if (self.board.lists[i].id == toList) { // source list
-                        self.board.lists[i].cards.splice(destCardIdx, 0, obj);
-                        notFound = false;
-                    }
-                }
-                self.board.lists[sourceListIdx].cards.splice(sourceCardIdx, 1);
-            }
-        });
     }
 }).$mount('#app');
--- public/scripts/vue-board.js
+++ public/scripts/vue-board.js
@@ -51,6 +51,39 @@ var mkanbanBoard = (function () {
                     }
                 }
             );
+
+            var self = this;
+            // event (dragula): card dropped
+            bus.$on("dragulaDrop", function (card, fromList, toList) {
+                console.log("[app]: event dragulaDrop")
+                var obj = null;
+                var sourceListIdx = -1;
+                var sourceCardIdx = -1;
+                var destCardIdx = 0;
+                // get card object from source list
+                for (var i = 0, notFound = true; i < self.board.lists.length && notFound; i++) {
+                    if (self.board.lists[i].id == fromList) { // source list
+                        sourceListIdx = i;
+                        for (var j = 0; j < self.board.lists[i].cards.length && notFound; j++) {
+                            if (self.board.lists[i].cards[j].id == card) { // card on source list
+                                sourceCardIdx = j;
+                                obj = self.board.lists[i].cards[j];
+                                notFound = false;
+                            }
+                        }
+                    }
+                }
+                if (obj) {
+                    for (var i = 0, notFound = true; i < self.board.lists.length && notFound; i++) {
+                        if (self.board.lists[i].id == toList) { // source list
+                            self.board.lists[i].cards.splice(destCardIdx, 0, obj);
+                            notFound = false;
+                        }
+                    }
+                    self.board.lists[sourceListIdx].cards.splice(sourceCardIdx, 1);
+                }
+            });
+
             this.$nextTick(() => this.$refs.listName.focus());
         }, mounted: function () {
             this.updateDragulaElements();
