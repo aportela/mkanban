e85977c0a59a6b081295c1724d948938664fde46
alex <766f6964+github@gmail.com>
2018-04-05 21:08:46 +0200

    return initial state vars on first request

--- src/AppRoutes.php
+++ src/AppRoutes.php
@@ -6,14 +6,35 @@
 
     $this->app->get('/', function (Request $request, Response $response, array $args) {
         $this->logger->info($request->getOriginalMethod() . " " . $request->getUri()->getPath());
+        $v = new \mKanban\Database\Version(new \mKanban\Database\DB($this), $this->get('settings')['database']['type']);
         return $this->view->render($response, 'index.html.twig', array(
             'settings' => $this->settings["twigParams"],
-            'initialState' => json_encode(array())
+            'initialState' => json_encode(
+                array(
+                    'upgradeAvailable' => $v->hasUpgradeAvailable()
+                )
+            )
         ));
     });
 
-    $this->app->get('/api/poll', function (Request $request, Response $response, array $args) {
-        return $response->withJson(['success' => true], 200);
+    $this->app->group("/api", function() {
+        $this->group("/boards", function() {
+            $this->get("/", function (Request $request, Response $response, array $args) {
+                $boards = \mKanban\Board::search();
+                return $response->withJson(['success' => true, 'boards' => $boards], 200);
+            });
+            $this->get("/{id}", function (Request $request, Response $response, array $args) {
+                $board = array(
+                    "id" => $args["id"],
+                    "name" => "default board",
+                    "lists" => array()
+                );
+                return $response->withJson(['success' => true, 'board' => $board], 200);
+            });
+        });
+        $this->get('/poll', function (Request $request, Response $response, array $args) {
+            return $response->withJson(['success' => true], 200);
+        });
     })->add(new \mKanban\Middleware\APIExceptionCatcher($this->app->getContainer()));
 
 ?>
