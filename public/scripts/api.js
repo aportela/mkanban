e6d9bfafddd31b4184c555c3513d3b37fcc0a1e0
alex <766f6964+github@gmail.com>
2018-05-06 21:57:38 +0200

    check callback type before call

--- public/scripts/api.js
+++ public/scripts/api.js
@@ -13,10 +13,14 @@ const mkanbanAPI = {
             }
             Vue.http.post("api/user/signup", params).then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         },
@@ -27,20 +31,28 @@ const mkanbanAPI = {
             }
             Vue.http.post("api/user/signin", params).then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         },
         signOut: function (callback) {
             Vue.http.get("api/user/signout").then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         }
@@ -49,20 +61,28 @@ const mkanbanAPI = {
         search: function (callback) {
             Vue.http.get("/boards.json").then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         },
         get: function (id, callback) {
             Vue.http.get("/board.json", { id: id }).then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         }
@@ -71,20 +91,28 @@ const mkanbanAPI = {
         add: function (card, callback) {
             Vue.http.get("/", card).then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         },
         get: function (id, callback) {
             Vue.http.get("/", { id: id }).then(
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 },
                 response => {
-                    callback(response);
+                    if (callback && typeof callback === "function") {
+                        callback(response);
+                    }
                 }
             );
         }
